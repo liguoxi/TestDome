0e3a6479737e29169d37e1375d33f5ff755a44ac
liguoxi <1063285760@qq.com>
2018-07-23 17:21:43 +0800

    测试代码冲突

--- ssm/src/main/java/com/liguoxi/Application.java
+++ ssm/src/main/java/com/liguoxi/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println("11111111111");
+        System.out.println("111111111112");
         SpringApplication.run(Application.class, args);
     }
 }
