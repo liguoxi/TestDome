613a4d53beea4f67867ebe489a8c9fc226a41083
liguoxi <31693237+liguoxi@users.noreply.github.com>
2018-07-23 17:28:11 +0800

    Update Application.java

--- ssm/src/main/java/com/liguoxi/Application.java
+++ ssm/src/main/java/com/liguoxi/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println("333333333333");
+        System.out.println("1111111111111111111111111111");
         SpringApplication.run(Application.class, args);
     }
 }
